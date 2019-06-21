5598a7d6f86bfee0fb61b2a50922b8693b78bddf
Jon Schulman <jon@js-tbpro13.local>
2019-06-21 14:07:51 -0700

    one more script update

--- main.tf
+++ main.tf
@@ -174,7 +174,7 @@ resource "aws_instance" "web" {
       "sudo /usr/bin/curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -",
       "sudo /usr/bin/apt install nodejs -y",
       "sudo /usr/bin/npm install -g @angular/cli@7.3.9",
-      "sudo cd /tmp/cas-demo-application && /usr/bin/npm install",
+      "cd /tmp/cas-demo-application && sudo /usr/bin/npm install",
       "sudo /usr/bin/ng build --prod",
       "sudo /bin/cp -R /tmp/cas-demo-application/dist/cmbu-demo-application/ /usr/share/nginx/html/",
       "sudo /bin/sed -i \"s@root /var/www/html@root /usr/share/nginx/html@\" /etc/nginx/sites-available/default",
