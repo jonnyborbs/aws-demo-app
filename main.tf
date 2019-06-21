5b9271400fbe260ab7565d70227406e6ec0d302d
Jon Schulman <jon@js-tbpro13.local>
2019-06-21 16:14:47 -0700

    finally got it

--- main.tf
+++ main.tf
@@ -177,7 +177,7 @@ resource "aws_instance" "web" {
       "sudo /usr/bin/npm install -g @angular/cli",
       "cd /tmp/cas-demo-application &&  /usr/bin/npm install",
       "/usr/bin/ng build --prod",
-      "sudo /bin/cp -R /tmp/cas-demo-application/dist/cmbu-demo-application/ /usr/share/nginx/html/",
+      "sudo /bin/cp -R /tmp/cas-demo-application/dist/cmbu-demo-application/* /usr/share/nginx/html/",
       "sudo /bin/sed -i \"s@root /var/www/html@root /usr/share/nginx/html@\" /etc/nginx/sites-available/default",
       "sudo /bin/systemctl restart nginx",
       "sudo ufw allow http",
