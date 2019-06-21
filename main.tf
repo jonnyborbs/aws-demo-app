a3fbe7a942c128aad950ace938a9f6e63410fa89
Jon Schulman <jon@js-tbpro13.local>
2019-06-21 12:47:08 -0700

    new provisioner

--- main.tf
+++ main.tf
@@ -164,9 +164,9 @@ resource "aws_instance" "web" {
   # this should be on port 80
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get -y update",
-      "sudo apt-get -y install nginx",
-      "sudo service nginx start",
+#      "sudo apt-get -y update",
+#      "sudo apt-get -y install nginx",
+#      "sudo service nginx start",
     ]
   }
 }
