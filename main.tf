0a604cf5e025a6ec038e4cfafe704dff5320bb8c
Jon Schulman <jon@js-tbpro13.local>
2019-06-25 14:43:51 -0700

    updated provisioner code

--- main.tf
+++ main.tf
@@ -165,7 +165,8 @@ resource "aws_instance" "web" {
   # this should be on port 80
   provisioner "remote-exec" {
     inline = [
-      "sudo apt install nginx curl git awscli -y",
+      "sudo apt install nginx curl git -y",
+      "sudo apt install awscli -y",
       "/usr/bin/git clone https://github.com/vaficionado/tf-demo-application /tmp/tf-demo-application",
       "/bin/rm -rf /etc/nginx/conf.d/",
       "/bin/rm -rf /usr/share/nginx/html/",
@@ -179,6 +180,7 @@ resource "aws_instance" "web" {
       "sudo /bin/sed -i \"s@root /var/www/html@root /usr/share/nginx/html@\" /etc/nginx/sites-available/default",
       "sudo /bin/systemctl restart nginx",
       "sudo ufw allow http",
+      "aws sns publish --target-arn ${var.slack_topic_name} --region us-east-1 --message "server provisioned at ip ${aws_instance.public_ip}"
     ]
   }
   tags = {
