ef9f730b04a912cb1d84b2bfb302c5e8abcce803
Jon Schulman <jon@js-tbpro13.local>
2019-06-27 15:30:42 -0700

    remove old comments

--- main.tf
+++ main.tf
@@ -111,10 +111,6 @@ resource "aws_elb" "web" {
   }
 }
 
-#resource "aws_key_pair" "auth" {
-#  key_name   = "${var.key_name}"
-#  public_key = "${var.public_key}"
-#}
 data "aws_ami" "ubuntu" {
     most_recent = true
 
